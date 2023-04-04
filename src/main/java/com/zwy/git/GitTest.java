3f58c3c85a1f31fb7dbf80f87fa01c433173c58a
Chastelovee <1092255381@qq.com>
2023-04-04 13:33:47 +0800

    hot-fix commit1

--- src/main/java/com/zwy/git/GitTest.java
+++ src/main/java/com/zwy/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello zwy!");
         System.out.println("Hello xjy!");
+        System.out.println("Hello Git4!");
     }
 }
