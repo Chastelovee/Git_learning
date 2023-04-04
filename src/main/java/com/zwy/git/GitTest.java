180adbbd7aafc707f5da234889ca5aa7380569a1
Chastelovee <1092255381@qq.com>
2023-04-04 13:37:28 +0800

    master commit2

--- src/main/java/com/zwy/git/GitTest.java
+++ src/main/java/com/zwy/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello zwy!");
         System.out.println("Hello xjy!");
         System.out.println("Hello Git4!");
+        System.out.println("master test!");
     }
 }
